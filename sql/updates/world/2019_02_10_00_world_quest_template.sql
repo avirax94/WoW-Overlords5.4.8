9292c09e09c88c993b96b87ac3757f6097908c21
Christian Echeverria <Avirax1994@gmail.com>
2020-08-20 01:49:23 -0500

    Overlords Mop
    2020 soucer code

---
+++ sql/updates/world/2019_02_10_00_world_quest_template.sql
@@ -0,0 +1,4 @@
+-- DB/Quest: Update: The Lesson of the Sandy Fist (29406)
+-- [Map: The Wandering Isle, Area: The Wandering Isle, Zone: Shang Xi Training Grounds]
+
+UPDATE `quest_template` SET `RewardSpellCast`=0, `RewardChoiceItemId1`=73214, `RewardChoiceItemId2`=73215, `RewardChoiceItemId3`=73216, `RewardChoiceItemCount1`=1, `RewardChoiceItemCount2`=1,`RewardChoiceItemCount3`=1 WHERE `id`=29406;
---
+++ sql/updates/world/2019_02_11_00_world_quest_template.sql
@@ -0,0 +1,4 @@
+-- DB/Quest: Update: The Lesson of Stifled Pride (29524)
+-- [Map: The Wandering Isle, Area: The Wandering Isle, Zone: Shang Xi Training Grounds]
+
+UPDATE `quest_template` SET `RewardSpellCast`=0, `RewardChoiceItemId1`=73217, `RewardChoiceItemId2`=73218, `RewardChoiceItemId3`=73219, `RewardChoiceItemCount1`=1, `RewardChoiceItemCount2`=1,`RewardChoiceItemCount3`=1 WHERE `id`=29524;
